0277152fdabb54594f320c8bac1dfb8272aa5a0a
senthurpandi@securenext.in <sathiyamurthy.dhanapal@crud-operations.com>
2022-09-28 00:40:58 +0530

    alter code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,21 @@ import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { from, range } from 'rxjs';
 import { IRouteCard } from './route-cards';
 
-
 import { RoutecardService } from './routecardservice';
+
+export interface IAlert{
+  type: 'inbom'|'outbom',
+  message: string;
+}
+
+export class Alert{
+  type: string;
+  message: string;
+  constructor(type: 'inbom'|'outbom' ,message: string){
+    this.type = type;
+    this.message = message;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -26,12 +39,11 @@ export class AppComponent {
   ) {}
 
   ngOnInit() {
-
     const count: number = 7;
 
     const [...array] = Array(count).keys();
 
-    this.routecards= this.routecardService.makeRowData(array);
+    this.routecards = this.routecardService.makeRowData(array);
 
     this.primengConfig.ripple = true;
     this.cols = [
@@ -56,105 +68,90 @@ export class AppComponent {
   }
 
   submit(): void {
-    let outbomCount = 0;
-    let inbomCount = 0;
 
+    const outbomCount = this.routecards.filter(
+      (value) => value.outbom === true
+    ).length;
 
-    let alertMsg: any[] = [];
+    const inbomCount = this.routecards.filter(
+      (value) => value.inbom === true
+    ).length;
+
+    let alertMessages: any[] = [];
     let breakOutbomIndex: any[] = [];
 
-    const lastArrayIndex = this.routecards.length - 1; // Last array index
-
-    this.routecards.forEach((x) => {
-      x.inbom === true ? inbomCount++ : '';
-      x.outbom === true ? outbomCount++ : '';
-    });
-
-    if (inbomCount === outbomCount) {
-      //Inward & Outward BOM check count to be equal
+    if (inbomCount === outbomCount) {  //Inward & Outward BOM check count to be equal
 
       if (
         this.routecards[0].outbom === true &&
-        this.routecards[lastArrayIndex].inbom === true
+        this.routecards[this.routecards.length - 1].inbom === true
       ) {
-        // 1st process Outward and last process Inward BOM Equal
 
-        let outBomArray: any[] = []
-        let inBomArray: any[] = []
-        this.routecards.map((ib,indx) => {
-          outBomArray.push(ib.outbom)
-          inBomArray.push(ib.inbom)
+      // 1st process Outward and last process Inward BOM Equal
+
+        let outBomArray: any[] = [];
+        let inBomArray: any[] = [];
+
+        this.routecards.map((ib) => {
+          outBomArray.push(ib.outbom);
+          inBomArray.push(ib.inbom);
         });
 
-
-
-
-       const bomLength = outBomArray.length - 1;
-
-        let endIndexOfInBom: number ;
+        let endIndexOfInBom: number;
         let endIndexOfInBomFlag = false;
 
-        outBomArray.forEach((outbom,startIndex) => {  //hole outbom each
-            if(outbom === true){
+        outBomArray.forEach((outbom, startIndex) => {
+          if (outbom === true) {
 
-              //logic
+            // find each end index of inbom based on outbom selection
 
-              inBomArray.forEach((element,inbomIndex) => {
-                if(bomLength > startIndex && inbomIndex >= startIndex){
-                  if(element === true && !endIndexOfInBomFlag){
-                    endIndexOfInBom = inbomIndex;
-                    endIndexOfInBomFlag = true;
-                  }
+            inBomArray.forEach((element, inbomIndex) => {
+              if (
+                (outBomArray.length - 1) > startIndex &&
+                inbomIndex >= startIndex
+              ) {
+                if ((element === true) && !endIndexOfInBomFlag) {
+                  endIndexOfInBom = inbomIndex;
+                  endIndexOfInBomFlag = true;
                 }
-              });   //get Successfully end index of inbom select
+              }
+            });
 
-
-              /* To find startIndex and endIndex have any true IN "OUTBOM"  */
+            /* To find startIndex and endIndex have any true IN "OUTBOM"  */
 
             let breakOutbomflag: boolean = false;
-              outBomArray.forEach((out,i) => {
-                if(i >= startIndex){
-                  if(i <= endIndexOfInBom){
-                     if( !(startIndex == i) && out === true && !breakOutbomflag){
-                      breakOutbomIndex.push(i);
-                      breakOutbomflag = true;
-                     }else{
+            outBomArray.forEach((out, i) => {
+              if (i >= startIndex && i <= endIndexOfInBom) {
+                  if (!(startIndex == i) && (out === true) && !breakOutbomflag) {
+                    breakOutbomIndex.push(i);          // if we select outbom must be select inbom . in between outbom again select mean find that index values
+                    breakOutbomflag = true;
+                  } else {
+                    if (this.routecards[i].outbom === null) {
+                        alertMessages.push(new Alert('outbom',`Please set outbom Cross ${i + 1}`));
+                    }
 
-                      if(this.routecards[i].outbom !== true){
-                        if(this.routecards[i].outbom !== false){
-                          const a = {
-                            type:'outbom',
-                            message: `Please set outbom Cross ${i+1} `
-                          }
-                          alertMsg.push(a);
-                        }
-                      }
-
-                      if(this.routecards[i].inbom !== true){
-                        if(this.routecards[i].inbom !== false){
-                          const a = {
-                            type:'inbom',
-                            message:  `Please set the inbom Cross ${i+1} `
-                          }
-                          alertMsg.push(a);
-                        }
-                      }
-                     }
+                    if (this.routecards[i].inbom === null) {
+                      alertMessages.push(new Alert('inbom',`Please set inbom Cross ${i + 1}`));
+                    }
                   }
-                }
-              });
-            }
-            endIndexOfInBomFlag = false;
+              }
+            });
+          }
+          endIndexOfInBomFlag = false;
         });
 
-        if(breakOutbomIndex.length === 0 ){
-          if(alertMsg.length === 0){
+        if (breakOutbomIndex.length === 0) {
+          if (alertMessages.length === 0) {
             this.showToast('success', 'Success', 'Form submit successfully!');
-          }else{
-            this.showToast('error', 'Error', `${alertMsg[0].message}`);
+          } else {
+            this.showToast('error', 'Error', `${alertMessages[0].message}`);
           }
-        }else{
-            this.showToast('error', 'Error', `${breakOutbomIndex[0]} remove that`);
+        } else {
+          this.showToast(
+            'error',
+            'Error',
+            `${breakOutbomIndex[0]} remove that`
+          );
         }
       } else {
         this.showToast(
@@ -168,82 +165,9 @@ export class AppComponent {
     }
   }
 
-
-
-  makeTableData(): any{
+  makeTableData(): any {
     const count: number = this.rowCount.value;
     const [...array] = Array(count).keys();
-    this.routecards= this.routecardService.makeRowData(array);
+    this.routecards = this.routecardService.makeRowData(array);
   }
 }
-
-       //-----------------------------------------//
-        // this.routecards.map((ib,indx) => {
-        //   if(ib.outbom === true){
-
-        //     if(ib.inbom === true){
-
-        //     }else{
-
-        //       let inbomFlag = true;
-        //       let notCrossedIndex;
-        //       let notCrossedBoolean = false;
-        //       this.routecards.forEach((item, index) => {
-
-        //         if(index > indx && inbomFlag && !notCrossedBoolean){
-
-        //           if(item.inbom === true)
-        //           inbomFlag = false;
-
-        //           let a = someFn(item,index,indx)
-
-        //           console.log(a,'sam')
-
-        //           if(a === 'notCrossed'){
-        //             notCrossedIndex = index;
-        //             notCrossedBoolean = true;
-        //             return ;
-        //           }
-
-        //         }
-        //       })
-        //     }
-        //   }
-        // })
-
-        //-----------------------------------------//
-
-        // //Continuous Outward
-        // let lastInwardCheck = true;
-        // let endPreviousInwardIndex;
-        // let isOrderTrueArray: any[] = [];
-        // this.routecards.map((x, index) => {
-        //   if (x.outbom === true) {
-        //     isOrderTrueArray.push(index); //push continious outbom true index values
-        //   } else {
-        //     if (isOrderTrueArray.length < 2) {
-        //       // is not be consecutive || is first time outbom false
-        //       isOrderTrueArray = [];
-        //     } else {
-        //       let lastTrueIndex = isOrderTrueArray[isOrderTrueArray.length - 1]; //if outbom consecutive get last index of outbom
-
-        //       if (this.routecards[lastTrueIndex - 1].inbom !== true) {
-        //         //check previous value inbom value
-        //         lastInwardCheck = false;
-        //         endPreviousInwardIndex = lastTrueIndex;
-        //       }
-
-        //       isOrderTrueArray = []; //Again set inital empty array
-        //     }
-        //   }
-        // });
-
-        // if (lastInwardCheck) {
-        //   this.showToast('success', 'Success', 'Form submit successfully!');
-        // } else {
-        //   this.showToast(
-        //     'error',
-        //     'Error',
-        //     `Row ${endPreviousInwardIndex} inbom must be check`
-        //   );
-        // }
